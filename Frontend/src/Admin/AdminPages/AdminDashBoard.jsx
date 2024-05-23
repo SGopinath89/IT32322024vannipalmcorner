d2c3906b3395fe57368bb118f2476b472b306f31
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-05-23 22:35:47 +0530

    modify admindashboard

--- Frontend/src/Admin/AdminPages/AdminDashBoard.jsx
+++ Frontend/src/Admin/AdminPages/AdminDashBoard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import AdminSidebar from '../Admincomponents/AdminSidebar';
+import Logo from '../image/logo.png';
+import './dashboardstyle.css';
 
 const AdminDashBoard = () => {
   return (
-    <>
-    <h1>Admin Dashboard</h1>
-    </>
-   
+    <div className='main'>
+      <div className='nav'>
+      <AdminSidebar></AdminSidebar>
+      </div>
+      <section className='section'>
+        <img src={Logo} alt="Logo" className='logo' />
+        <div className='content'>
+          <h1>Vanni Palm Corner</h1>
+          <p>Discover Crafted Vanni Palm Treasures!!!!</p>
+        </div>
+
+
+      </section>
+
+
+    </div>
+
   );
 };
 
