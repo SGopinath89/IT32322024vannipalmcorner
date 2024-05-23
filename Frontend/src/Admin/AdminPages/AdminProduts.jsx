74b7835b808cd40b060a080e936c1e359788883b
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-05-23 22:38:01 +0530

    modify adminprodutspage

--- Frontend/src/Admin/AdminPages/AdminProduts.jsx
+++ Frontend/src/Admin/AdminPages/AdminProduts.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import AdminSidebar from '../Admincomponents/AdminSidebar';
+import './pagestyle.css';
+
+import Logo from '../image/logo.png';
 
 const AdminProducts = () => {
   return (
-    <>
-    <h1>Produts Details</h1>
-    </>
-   
+    <div className='main'>
+      <div className='nav'>
+        <AdminSidebar></AdminSidebar>
+      </div>
+      <section className='section'>
+        <div className='head'>
+          <h1>Products details </h1>
+        </div>
+        <div className='detail'>
+          <div className='data'></div>
+          <div className='many'></div>
+        </div>
+      </section>
+
+
+    </div>
+
   );
 };
 
