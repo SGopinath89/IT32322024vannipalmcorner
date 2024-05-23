8410b409899afa67d78ddb93baa2273c3bf6ec9c
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-05-23 22:43:16 +0530

    add sample data file

---
+++ Frontend/src/Admin/data.js
@@ -0,0 +1,36 @@
+
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@admin.com',
+    password:'admin123',
+    isAdmin: true
+  },
+  {
+    name: 'John Doe',
+    email: 'john@email.com',
+    password:'john123',
+    isAdmin: false
+  },
+  {
+    name: 'Alice Smith',
+    email: 'alice@email.com',
+    password:'alice123',
+    isAdmin: false
+  },
+  {
+    name: 'Eva Brown',
+    email: 'eva@email.com',
+    password:'eva123',
+    isAdmin: false
+  },
+  {
+    name: 'David Miller',
+    email: 'david@email.com',
+    password:'david123',
+    isAdmin: false
+  }
+];
+
+export default users;
