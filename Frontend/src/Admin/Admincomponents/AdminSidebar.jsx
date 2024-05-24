eecf38df9840b5df7a7faa0a48c8443b8589a4d4
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-05-24 21:25:17 +0530

    add navigate function

--- Frontend/src/Admin/Admincomponents/AdminSidebar.jsx
+++ Frontend/src/Admin/Admincomponents/AdminSidebar.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import { Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import './adminsidebarstyle.css'
+import React, { useTransition } from 'react'; // Import only useTransition
+import { useNavigate } from 'react-router-dom';
+import './adminsidebarstyle.css';
 
 const AdminSidebar = () => {
+  const navigate = useNavigate();
+  const [isPending, startTransition] = useTransition(); // Destructure correctly
+
+  const handleUser = () => {
+    startTransition(() => {
+      navigate('/adminuser');
+    });
+  };
+
+  const handleProduct = () => {
+    startTransition(() => {
+      navigate('/adminproduct');
+    });
+  };
+
+  const handleSeller = () => {
+    startTransition(() => {
+      navigate('/adminseller');
+    });
+  };
+
   return (
     <>
-    <div className='sidebar'>
-        <div className='users'>
-            <button>Users</button>
+      <div className='sidebar'>
+        <div className='user'>
+          <button type='button' onClick={handleUser}>Users</button>
         </div>
         <div className='sellers'>
-            <button>Sellers</button>
+          <button type='button' onClick={handleSeller}>Sellers</button>
         </div>
         <div className='products'>
-            <button>Products</button>
+          <button type='button' onClick={handleProduct}>Products</button>
         </div>
-
-    </div>
+      </div>
+      {isPending && <div>Loading...</div>} {/* Optional: Show loading indicator */}
     </>
-   
   );
 };
 
