c3f6a12713c2c618945cc391bd9fac9226bf63d0
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-05-24 21:22:40 +0530

    modify admin seller page

--- Frontend/src/Admin/AdminPages/AdminSellers.jsx
+++ Frontend/src/Admin/AdminPages/AdminSellers.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import AdminSidebar from '../Admincomponents/AdminSidebar';
-
+import './pagestyle.css';
 
 const AdminSellers = () => {
   return (
-    <>
-    <AdminSidebar></AdminSidebar>
-    </>
+    <div className='main'>
+      <div className='nav'>
+        <AdminSidebar></AdminSidebar>
+      </div>
+      <section className='section'>
+        <div className='head'>
+          <h1>Seller details </h1>
+        </div>
+        <div className='detail'>
+          <div className='data'>
+            <input type='search'></input>
+            <div className='owndetail'>
+              <img></img>
+              <h3> Name: </h3>
+            </div>
+          </div>
+          <div className='many'>
+
+          </div>
+        </div>
+      </section>
+
+
+    </div>
    
   );
 };
